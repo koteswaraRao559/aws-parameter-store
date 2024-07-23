5a34df91ffacd6d52d5900628512f600bf096755
koteswaraRao559 <koteswararao1999@gmail.com>
2024-07-23 20:37:47 +0530

    4th commit

--- vars.tf
+++ vars.tf
@@ -1,8 +1,13 @@
 variable "parameters" {
   default = [
     { name = "ses.username" , type = "String", value = "ses-smtp-user.20240528-163905" },
-    { name = "dev.rds.username" , type = "String", value = "admin1" },
     { name = "dev.backend.DB_HOST" , type = "String", value = "dev-mysql-rds.c50m0cciccxq.us-east-1.rds.amazonaws.com" },
+    { name = "dev.frontend.BACKEND_ENDPOINT" , type = "String", value = "backend-dev.kdevops59.online" },
+
+
+    ##rds
+    { name = "dev.rds.username" , type = "String", value = "admin1" },
+    { name = "dev.rds.endpoint" , type = "String", value = "dev-mysql-rds.c50m0cciccxq.us-east-1.rds.amazonaws.com" },
 
 
     ### Passwords,
